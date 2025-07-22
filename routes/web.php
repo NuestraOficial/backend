19e54d454e9249f25e4cf65a59de8024954095b9
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-07-21 21:39:47 -0300

    style: mensagens personalizadas para o meu amor

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage/{filename}', function ($filename) {
+    $path = storage_path('app/public/' . $filename);
+
+    if (!file_exists($path)) {
+        abort(404);
+    }
+
+    return response()->file($path);
+});
